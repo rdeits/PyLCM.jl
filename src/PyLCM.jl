2b86df682060e59a8a5b8711ff2afa8ca01d96d5
Robin Deits <robin.deits@gmail.com>
2016-02-26 14:23:56 -0500

    initial commit

--- src/PyLCM.jl
+++ src/PyLCM.jl
@@ -1,5 +1,32 @@
 module PyLCM
 
-# package code goes here
+export LCM, publish, subscribe, handle
+
+using PyCall
+@pyimport lcm as pylcm
+
+immutable PyLCMWrapper
+	lcm_obj::PyObject
+end
+
+function LCM()
+	PyLCMWrapper(pylcm.LCM())
+end
+
+function publish(lc::PyLCMWrapper, channel::AbstractString, msg)
+	pycall(lc.lcm_obj[:publish], PyAny, channel, pycall(msg[:encode], PyObject))
+end
+
+function subscribe(lc::PyLCMWrapper, channel::AbstractString, handler::Function)
+	lc.lcm_obj[:subscribe](channel, pyeval("lambda chan, data, handler=h: handler(chan, bytearray(data))", h=handler))
+end
+
+function subscribe(lc::PyLCMWrapper, channel::AbstractString, handler::Function, msg_type::PyObject)
+	lc.lcm_obj[:subscribe](channel, pyeval("lambda chan, data, handler=h, msg_type=t: handler(chan, msg_type.decode(data))", h=handler, t=msg_type))
+end
+
+function handle(lc::PyLCMWrapper)
+	pycall(lc.lcm_obj[:handle], PyObject)
+end
 
 end # module
---
+++ travis/setup_lcm.sh
@@ -0,0 +1,33 @@
+set -e
+
+if [ "$TRAVIS_OS_NAME" = "linux" ]
+	then
+	sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
+	sudo apt-get update -qq
+	sudo apt-get install -y build-essential libglib2.0-dev wget
+elif [ "$TRAVIS_OS_NAME" = "osx" ]
+	then
+	brew update
+	brew install glib pkg-config wget
+else
+	echo "ERROR: TRAVIS_OS_NAME: $TRAVIS_OS_NAME is not handled, not installing LCM"
+	exit(1)
+fi
+
+wget https://github.com/lcm-proj/lcm/releases/download/v1.3.0/lcm-1.3.0.zip
+unzip lcm-1.3.0.zip
+cd lcm-1.3.0.zip
+./configure
+make
+
+if [ "$TRAVIS_OS_NAME" = "linux" ]
+	then
+	sudo make install
+elif [ "$TRAVIS_OS_NAME" = "osx" ]
+	then
+	make install
+fi
+
+cd ..
+lcm-gen -p lcm-1.3.0/examples/multidim_array_t.lcm
+
