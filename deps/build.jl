46cc99adb521015d2a9755652b4ac4e628523051
Robin Deits <robin.deits@gmail.com>
2016-03-12 00:11:14 -0500

    build sort of works, but doesn't set python path

--- deps/build.jl
+++ deps/build.jl
@@ -1,15 +1,21 @@
 using BinDeps
+using Compat
 
 @BinDeps.setup
 
 deps = [
 	gobject = library_dependency("gobject", aliases = ["libgobject-2.0-0", "libgobject-2.0"])
-	lcm = library_dependency("lcm", aliases=["liblcm", "liblcm.so.1", "liblcm.so.1.3.4"])
+	lcm = library_dependency("lcm", aliases=["liblcm", "liblcm.1", "liblcm.1.3.4"])
 	]
 	# pkgconfig = library_dependency("pkgconfig", aliases = ["pkg-config"])
 
 @linux_only begin
 	append!(deps, [java6 = library_dependency("openjdk-6-jdk")])
+	provides(AptGet,
+		Dict("libglib2.0-dev" => gobject,
+		     "openjdk-6-jdk" => java6
+		 # "build-essential" => pkgconfig
+		 ))
 end
 
 @osx_only begin
@@ -18,11 +24,6 @@ end
 	# provides(Homebrew.HB, "pkg-config", pkgconfig, os=:Darwin)
 end
 
-provides(AptGet,
-	Dict("libglib2.0-dev" => gobject,
-	     "openjdk-6-jdk" => java6
-	 # "build-essential" => pkgconfig
-	 ))
 
 provides(Yum,
     Dict("glib" => gobject,
@@ -33,7 +34,7 @@ provides(Sources,
 	Dict(URI("https://github.com/lcm-proj/lcm/releases/download/v1.3.0/lcm-1.3.0.zip") => lcm))
 
 provides(BuildProcess,
-	Dict(Autotools() => lcm))
+	Dict(Autotools(libtarget="lcm/liblcm.la"*BinDeps.shlib_ext) => lcm))
 
-@BinDeps.install
+@BinDeps.install @compat Dict(:lcm => :lcm)
 
