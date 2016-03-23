f1b2ea7f056d40a4a4e9d98d097a518064ac2a5a
Robin Deits <robin.deits@gmail.com>
2016-03-22 22:39:31 -0400

    fix bindeps build and add pythonpath hook

--- deps/build.jl
+++ deps/build.jl
@@ -4,37 +4,45 @@ using Compat
 @BinDeps.setup
 
 deps = [
-	gobject = library_dependency("gobject", aliases = ["libgobject-2.0-0", "libgobject-2.0"])
-	lcm = library_dependency("lcm", aliases=["liblcm", "liblcm.1", "liblcm.1.3.4"])
-	]
-	# pkgconfig = library_dependency("pkgconfig", aliases = ["pkg-config"])
+    gobject = library_dependency("gobject", aliases = ["libgobject-2.0-0", "libgobject-2.0", "libgobject-2_0-0", "libgobject-2.0.so.0"])
+    lcm = library_dependency("lcm", aliases=["liblcm", "liblcm.1"], depends=[gobject])
+]
 
-@linux_only begin
-	append!(deps, [java6 = library_dependency("openjdk-6-jdk")])
-	provides(AptGet,
-		Dict("libglib2.0-dev" => gobject,
-		     "openjdk-6-jdk" => java6
-		 # "build-essential" => pkgconfig
-		 ))
-end
-
+prefix = joinpath(BinDeps.depsdir(lcm), "usr")
 @osx_only begin
-	using Homebrew
-	provides(Homebrew.HB, "glib", gobject, os=:Darwin)
-	# provides(Homebrew.HB, "pkg-config", pkgconfig, os=:Darwin)
+    if Pkg.installed("Homebrew") === nothing
+        error("Homebrew package not installed, please run Pkg.add(\"Homebrew\")")
+    end
+    using Homebrew
+    provides(Homebrew.HB, "glib", gobject, os=:Darwin)
+    pkg_config_path = joinpath(Homebrew.prefix(), "lib", "pkgconfig")
+    ENV["PKG_CONFIG_PATH"] = get(ENV, "PKG_CONFIG_PATH", "") * pkg_config_path
+    ENV["INCLUDE_PATH"] = get(ENV, "INCLUDE_PATH", "") * joinpath(Homebrew.prefix(), "include")
 end
 
+provides(AptGet, Dict("libglib2.0-dev" => gobject))
 
 provides(Yum,
-    Dict("glib" => gobject,
-     # "pkgconfig" => pkgconfig
-     ))
+    Dict("glib" => gobject))
 
 provides(Sources,
-	Dict(URI("https://github.com/lcm-proj/lcm/releases/download/v1.3.0/lcm-1.3.0.zip") => lcm))
+    URI("https://github.com/lcm-proj/lcm/releases/download/v1.3.0/lcm-1.3.0.zip"),
+    lcm)
 
-provides(BuildProcess,
-	Dict(Autotools(libtarget="lcm/liblcm.la"*BinDeps.shlib_ext) => lcm))
+provides(SimpleBuild,
+    (@build_steps begin
+        GetSources(lcm)
+        @build_steps begin
+            ChangeDirectory(joinpath(BinDeps.depsdir(lcm), "src", "lcm-1.3.0"))
+            `./configure --prefix=$(prefix) --with-java=no` # disable java due to https://github.com/lcm-proj/lcm/issues/56
+            MakeTargets(".", [])
+            MakeTargets(".", ["install"])
+        end
+    end), lcm, onload="""
+using PyCall
+@pyimport sys
+unshift!(PyVector(pyimport("sys")["path"]), joinpath("$(prefix)", "lib", "python" * string(sys.version_info[:major]) * "." * string(sys.version_info[:minor]), "site-packages"))
+"""
+)
 
-@BinDeps.install @compat Dict(:lcm => :lcm)
-
+@BinDeps.install Dict(:lcm => :liblcm)
--- travis/setup_lcm.sh
+++ travis/setup_lcm.sh
@@ -1,18 +1,18 @@
 set -e
 
-if [ "$TRAVIS_OS_NAME" = "linux" ]
-	then
-	sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
-	sudo apt-get update -qq
-	sudo apt-get install -y build-essential libglib2.0-dev python-dev python3-dev openjdk-6-jdk wget
-elif [ "$TRAVIS_OS_NAME" = "osx" ]
-	then
-	brew update > brew_update.log
-	brew install glib
-else
-	echo "ERROR: TRAVIS_OS_NAME: $TRAVIS_OS_NAME is not handled, not installing LCM"
-	exit 1
-fi
+# if [ "$TRAVIS_OS_NAME" = "linux" ]
+# 	then
+# 	sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
+# 	sudo apt-get update -qq
+# 	sudo apt-get install -y build-essential libglib2.0-dev python-dev python3-dev openjdk-6-jdk wget
+# elif [ "$TRAVIS_OS_NAME" = "osx" ]
+# 	then
+# 	brew update > brew_update.log
+# 	brew install glib
+# else
+# 	echo "ERROR: TRAVIS_OS_NAME: $TRAVIS_OS_NAME is not handled, not installing LCM"
+# 	exit 1
+# fi
 
 # wget https://github.com/lcm-proj/lcm/releases/download/v1.3.0/lcm-1.3.0.zip
 # unzip lcm-1.3.0.zip > unzip_lcm.log
