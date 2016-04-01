d0dea0aeb9d10c36eee153219890ef79289870d3
Robin Deits <robin.deits@gmail.com>
2016-04-01 15:02:32 -0400

    simplify PyLCMWrapper to LCM

--- src/PyLCM.jl
+++ src/PyLCM.jl
@@ -5,36 +5,33 @@ isfile(depsjl) ? include(depsjl) : error("PyLCM not properly ",
     "installed. Please run\nPkg.build(\"PyLCM\")")
 
 export LCM, publish, subscribe, handle
-global pylcm
 
 using PyCall
 
-immutable PyLCMWrapper
+immutable LCM
 	lcm_obj::PyObject
+
+	LCM() = new(pylcm[:LCM]())
 end
 
-function LCM()
-	PyLCMWrapper(pylcm[:LCM]())
-end
-
-function publish(lc::PyLCMWrapper, channel::AbstractString, msg)
+function publish(lc::LCM, channel::AbstractString, msg)
 	pycall(lc.lcm_obj[:publish], PyAny, channel, pycall(msg[:encode], PyObject))
 end
 
-function subscribe(lc::PyLCMWrapper, channel::AbstractString, handler::Function)
+function subscribe(lc::LCM, channel::AbstractString, handler::Function)
 	lc.lcm_obj[:subscribe](channel, pyeval("lambda chan, data, handler=h: handler(chan, bytearray(data))", h=handler))
 end
 
-function subscribe(lc::PyLCMWrapper, channel::AbstractString, handler::Function, msg_type::PyObject)
+function subscribe(lc::LCM, channel::AbstractString, handler::Function, msg_type::PyObject)
 	lc.lcm_obj[:subscribe](channel, pyeval("lambda chan, data, handler=h, msg_type=t: handler(chan, msg_type.decode(data))", h=handler, t=msg_type))
 end
 
-function handle(lc::PyLCMWrapper)
+function handle(lc::LCM)
 	pycall(lc.lcm_obj[:handle], PyObject)
 end
 
 function __init__()
-	global pylcm = pyimport("lcm")
+	const global pylcm = pyimport("lcm")
 end
 
 end # module
