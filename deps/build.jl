64737fef5e2b183b64cf47c85e01d00e457fe191
Robin Deits <robin.deits@gmail.com>
2016-05-23 13:39:32 -0400

    change gobject references to glib, which is what we really need

--- deps/build.jl
+++ deps/build.jl
@@ -4,8 +4,8 @@ using Compat
 @BinDeps.setup
 
 deps = [
-    gobject = library_dependency("gobject", aliases = ["libgobject-2.0-0", "libgobject-2.0", "libgobject-2_0-0", "libgobject-2.0.so.0"])
-    lcm = library_dependency("lcm", aliases=["liblcm", "liblcm.1"], depends=[gobject])
+    glib = library_dependency("glib", aliases = ["libglib-2.0-0", "libglib-2.0", "libglib-2_0-0", "libglib-2.0.so.0"])
+    lcm = library_dependency("lcm", aliases=["liblcm", "liblcm.1"], depends=[glib])
 ]
 
 prefix = joinpath(BinDeps.depsdir(lcm), "usr")
@@ -14,16 +14,16 @@ prefix = joinpath(BinDeps.depsdir(lcm), "usr")
         error("Homebrew package not installed, please run Pkg.add(\"Homebrew\")")
     end
     using Homebrew
-    provides(Homebrew.HB, "glib", gobject, os=:Darwin)
+    provides(Homebrew.HB, "glib", glib, os=:Darwin)
     pkg_config_path = joinpath(Homebrew.prefix(), "lib", "pkgconfig")
     ENV["PKG_CONFIG_PATH"] = get(ENV, "PKG_CONFIG_PATH", "") * pkg_config_path
     ENV["INCLUDE_PATH"] = get(ENV, "INCLUDE_PATH", "") * joinpath(Homebrew.prefix(), "include")
 end
 
-provides(AptGet, Dict("libglib2.0-dev" => gobject))
+provides(AptGet, Dict("libglib2.0-dev" => glib))
 
 provides(Yum,
-    Dict("glib" => gobject))
+    Dict("glib" => glib))
 
 provides(Sources,
     URI("https://github.com/lcm-proj/lcm/releases/download/v1.3.1/lcm-1.3.1.zip"),
