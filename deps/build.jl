8a7fb9b167781b2bc010a2c4b54141cd3e70f560
Robin Deits <robin.deits@gmail.com>
2016-10-07 18:05:36 -0400

    try to switch to new lcm cmake build

--- deps/build.jl
+++ deps/build.jl
@@ -14,28 +14,26 @@ function cflags_validator(pkg_name)
     end
 end
 
-@linux? (
-    begin
-        deps = [
-            python = library_dependency("python", aliases=["libpython2.7.so", "libpython3.2.so", "libpython3.3.so", "libpython3.4.so", "libpython3.5.so", "libpython3.6.so", "libpython3.7.so"], validate=cflags_validator("python"))
-            glib = library_dependency("glib", aliases=["libglib-2.0-0", "libglib-2.0", "libglib-2.0.so.0"], depends=[python], validate=cflags_validator("glib-2.0"))
-            lcm = library_dependency("lcm", aliases=["liblcm", "liblcm.1"], depends=[glib])
+@static if is_linux()
+    deps = [
+        python = library_dependency("python", aliases=["libpython2.7.so", "libpython3.2.so", "libpython3.3.so", "libpython3.4.so", "libpython3.5.so", "libpython3.6.so", "libpython3.7.so"], validate=cflags_validator("python"))
+        glib = library_dependency("glib", aliases=["libglib-2.0-0", "libglib-2.0", "libglib-2.0.so.0"], depends=[python], validate=cflags_validator("glib-2.0"))
+        lcm = library_dependency("lcm", aliases=["liblcm", "liblcm.1"], depends=[glib])
 
-            provides(AptGet, Dict("python-dev" => python, "libglib2.0-dev" => glib))
-        ]
-    end
-    : begin
-        deps = [
-            glib = library_dependency("glib", aliases = ["libglib-2.0-0", "libglib-2.0", "libglib-2.0.so.0"])
-            lcm = library_dependency("lcm", aliases=["liblcm", "liblcm.1"], depends=[glib])
-        ]
-    end
-    )
+        provides(AptGet, Dict("python-dev" => python, "libglib2.0-dev" => glib))
+    ]
+else
+    deps = [
+        glib = library_dependency("glib", aliases = ["libglib-2.0-0", "libglib-2.0", "libglib-2.0.so.0"])
+        lcm = library_dependency("lcm", aliases=["liblcm", "liblcm.1"], depends=[glib])
+    ]
+end
 
 prefix = joinpath(BinDeps.depsdir(lcm), "usr")
 pkg_config_dirs = AbstractString[]
 include_dirs = AbstractString[]
-@osx_only begin
+
+@static if is_apple()
     if Pkg.installed("Homebrew") === nothing
         error("Homebrew package not installed, please run Pkg.add(\"Homebrew\")")
     end
@@ -48,11 +46,29 @@ end
 provides(Yum,
     Dict("glib" => glib))
 
-provides(Sources,
-    URI("https://github.com/lcm-proj/lcm/releases/download/v1.3.1/lcm-1.3.1.zip"),
-    lcm)
+lcm_sha = "9e53469cd0713ca8fbf37a968f6fd314f5f11584"
+lcm_folder = "lcm-$(lcm_sha)"
 
-provides(BuildProcess, Dict(Autotools(libtarget="lcm/liblcm.la", include_dirs=include_dirs, pkg_config_dirs=pkg_config_dirs) => lcm), onload="""
+provides(Sources,
+    URI("https://github.com/lcm-proj/lcm/archive/$(lcm_sha).zip"),
+    lcm,
+    unpacked_dir=lcm_folder)
+
+lcm_builddir = joinpath(BinDeps.depsdir(lcm), "build")
+lcm_srcdir = joinpath(BinDeps.depsdir(lcm), "src", lcm_folder)
+
+provides(BuildProcess,
+    (@build_steps begin
+        GetSources(lcm)
+        CreateDirectory(lcm_builddir)
+        @build_steps begin
+            ChangeDirectory(lcm_builddir)
+            `cmake -DCMAKE_INSTALL_PREFIX="$(prefix)" $(lcm_srcdir)`
+            `cmake --build . --target install`
+        end
+    end),
+    lcm,
+    onload="""
 using PyCall
 sys = pyimport("sys")
 unshift!(PyVector(sys["path"]), joinpath("$(prefix)", "lib", "python" * string(sys[:version_info][1]) * "." * string(sys[:version_info][2]), "site-packages"))
